254e01e9557db915c4dae785d2570d3ea90a211a
David Sanz <davidsanzraya@gmail.com>
2023-10-21 20:15:35 +0200

    Ejercicios 11-12-13

---
+++ Ejercicio_13/app/src/main/java/com/example/ejercicio_13/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ejercicio_13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Registered", Toast.LENGTH_LONG).show()
+        }
+    }
+}
