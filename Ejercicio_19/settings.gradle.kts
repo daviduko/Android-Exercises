4bef542bc8b94b284a78119465ec59e2f7c119fe
David Sanz <davidsanzraya@gmail.com>
2023-10-22 19:05:42 +0200

    Ejercicios 17-18-19-20

---
+++ Ejercicio_19/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ejercicio_19"
+include(":app")
+ 
